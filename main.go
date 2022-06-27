fc660cd301dec26001ed00e8c6e9934a12465a88
K019C1262 <k019c1262@g.neec.ac.jp>
2022-06-27 18:58:53 +0900

    サンプルデータを差し替えた

--- main.go
+++ main.go
@@ -4,21 +4,23 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-    "github.com/gin-contrib/cors"
 )
 
 type photo struct {
     ID string `json:"id"`
     UserID string `json:"user_id"`
     PostDate string `json:"post_date"`
-    Tag string `json:"tag"`
+    Tag []string  `json:"tag"`
     URL string `json:"url"`
 }
 
 var photos = []photo{
-    {ID: "00000001", UserID: "0001", PostDate: "2022.04.20", Tag: "#りんご,#山形りんご", URL: "https://columbia.jp/idolmaster/img/COCC-18008.jpg"},
-    {ID: "00000002", UserID: "0001", PostDate: "2022.04.20", Tag: "#すだち,#徳島すだち", URL: "https://columbia.jp/idolmaster/img/COCC-18009.jpg"},
+    {ID: "00000001", UserID: "0001", PostDate: "2022.04.20", Tag: []string{"クレープ", "いちご"}, URL: "https://cdn.discordapp.com/attachments/755257997789233195/990916146825920522/DSC_00012_23.jpeg"},
+    {ID: "00000002", UserID: "0001", PostDate: "2022.05.11", Tag: []string{"トースト", "あずき"}, URL: "https://cdn.discordapp.com/attachments/755257997789233195/990916205638484038/DSC_00012_15.jpeg"},
+    {ID: "00000003", UserID: "0001", PostDate: "2022.06.20", Tag: []string{"生ハム", "ワイン", "白"}, URL: "https://cdn.discordapp.com/attachments/755257997789233195/990916206804488222/DSC_0200.jpg"},
+    {ID: "00000004", UserID: "0001", PostDate: "2022.06.21", Tag: []string{"海鮮丼", "サーモン", "いくら"}, URL: "https://cdn.discordapp.com/attachments/755257997789233195/990916227989897226/DSC_0064.jpg"},
 }
 
 func main() {
